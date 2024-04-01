5d556c962966cbfe67bad1277e42a17c401fdad9
karthick G <karthick.g@worktual.com>
2024-04-01 10:10:22 +0530

    Initial Commit

---
+++ DataOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataOrganizer
+//
+//  Created by VC on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
